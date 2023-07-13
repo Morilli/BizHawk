468cdaa2c2b65e40f5b69a1115e99525ba621933
Fortranm <fortranm@gmail.com>
2023-07-13 02:07:41 -0700

    fix SGB conversion in LsmvImport

--- src/BizHawk.Client.Common/movie/import/LsmvImport.cs
+++ src/BizHawk.Client.Common/movie/import/LsmvImport.cs
@@ -149,7 +149,7 @@ namespace BizHawk.Client.Common.movie.import
 							break;
 						case "sgb_ntsc":
 						case "sgb_pal":
-							platform = VSystemID.Raw.SNES;
+							platform = VSystemID.Raw.SGB;
 							break;
 					}
 
