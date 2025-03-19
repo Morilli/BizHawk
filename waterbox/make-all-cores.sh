44cd9b5280dcc8c30661783ca1994163730b77eb
Morilli <35152647+Morilli@users.noreply.github.com>
2025-03-19 23:39:57 +0100

    debug CI

--- waterbox/make-all-cores.sh
+++ waterbox/make-all-cores.sh
@@ -4,21 +4,21 @@ cd "$(dirname "$0")"
 
 make -C emulibc $1 -j
 make -C libco $1 -j
-./ares64/make-both.sh $1
-make -C bsnescore $1 -j
+# ./ares64/make-both.sh $1
+# make -C bsnescore $1 -j
 make -C dosbox $1 -j8
-make -C dsda $1 -j
-make -C gpgx $1 -j
-make -C libsnes $1 -j
-make -C melon $1 -j
-make -C picodrive $1 -j
-make -C stella $1 -j
-make -C snes9x $1 -j
-make -C tic80 $1 -j
-make -C uae $1 -j
-make -C uzem $1 -j
-make -C virtualjaguar $1 -j
-./nyma/make-all-released-cores.sh $1
+# make -C dsda $1 -j
+# make -C gpgx $1 -j
+# make -C libsnes $1 -j
+# make -C melon $1 -j
+# make -C picodrive $1 -j
+# make -C stella $1 -j
+# make -C snes9x $1 -j
+# make -C tic80 $1 -j
+# make -C uae $1 -j
+# make -C uzem $1 -j
+# make -C virtualjaguar $1 -j
+# ./nyma/make-all-released-cores.sh $1
 
 # this won't include MAME by default, due to the large amount of time it takes to build it
 # to include MAME just do INCLUDE_MAME=1 ./make-all-cores.sh
